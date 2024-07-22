6c7bbf0f0495b836bcfc211a5287d4e267254a45
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-07-22 18:41:16 +0900

    Feat: Changed API to get Data for each tag

--- internal/handler/homeRecommendation.go
+++ internal/handler/homeRecommendation.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/protobuf/types/known/structpb"
 	"log"
@@ -36,93 +35,69 @@ type HomeResponse struct {
 // @Success      200 {object} BaseResponse{data=[]HomeResponse} "성공"
 // @Router       /recommend/tags [post]
 func (pineconeHandler *PineconeHandler) HomeRecommendation(c *gin.Context) {
+	// HomeRequest 형식으로 입력을 받습니다
 	request := &HomeRequest{}
 	if err := c.ShouldBindJSON(&request); err != nil {
 		BaseResponse(c, http.StatusBadRequest, "error - "+err.Error(), nil)
 		return
 	}
 
-	//
+	// 한국어 태그가 들어오면 영어태그로 할당합니다
 	englishTags, err := mapTagsKoreanToEnglish(request.Tags)
 	if err != nil {
 		BaseResponse(c, http.StatusBadRequest, "error - "+err.Error(), nil)
 		return
 	}
-
-	// 필터링 조건을 설정합니다.
-	filterConditions := make([]*structpb.Value, len(englishTags))
-	for i, tag := range englishTags {
-		filterConditions[i] = structpb.NewStructValue(&structpb.Struct{
-			Fields: map[string]*structpb.Value{
-				"ssss": {
-					Kind: &structpb.Value_StringValue{
-						StringValue: tag,
-					},
-				},
-			},
-		})
-		fmt.Print(filterConditions[i])
-	}
-
-	filterStruct := &structpb.Struct{
-		Fields: map[string]*structpb.Value{
-			"$or": {
-				Kind: &structpb.Value_ListValue{
-					ListValue: &structpb.ListValue{
-						Values: filterConditions,
-					},
-				},
-			},
-		},
-	}
-	fmt.Println(filterStruct)
-
-	values, err := pineconeHandler.queryPineconeWithTag(filterStruct)
-	if err != nil {
-		BaseResponse(c, http.StatusInternalServerError, "error - "+err.Error(), nil)
-		return
-	}
-
-	// 반환된 벡터의 ID를 수집합니다.
-	returnSongs := make([]songHomeResponse, 0, len(values.Matches))
-	for _, match := range values.Matches {
-		v := match.Vector
-		songNumber, err := strconv.Atoi(v.Id)
-		if err != nil {
-			log.Printf("Failed to convert ID to int, error: %+v", err)
-		}
-		koreanTags, err := mapTagsEnglishToKorean(parseTags(v.Metadata.Fields["ssss"].GetStringValue()))
-		if err != nil {
-			log.Printf("Failed to convert tags to korean, error: %+v", err)
-			koreanTags = []string{}
-		}
-		returnSongs = append(returnSongs, songHomeResponse{
-			songNumber,
-			v.Metadata.Fields["song_name"].GetStringValue(),
-			v.Metadata.Fields["singer_name"].GetStringValue(),
-			koreanTags,
-		})
-	}
-
-	// []songResponse를 request.Tags(한국어태그)에 들어있는 태그들로 분류해서 []HomeResponse로 변환하는 코드
-	tagSongMap := make(map[string][]songHomeResponse)
-	for _, song := range returnSongs {
-		for _, tag := range song.Tags {
-			tagSongMap[tag] = append(tagSongMap[tag], song)
-		}
-	}
-
+	// 각 태그에 대해서 돌면서 값을 가져온다!
+	filterConditions := make([]*structpb.Struct, len(englishTags))
+	//[]songResponse를 request.Tags(한국어태그)에 들어있는 태그들로 분류해서 []HomeResponse로 변환하는 코드
 	var homeResponses []HomeResponse
-	for _, tag := range request.Tags {
-		songs := tagSongMap[tag]
-		if songs == nil {
-			songs = []songHomeResponse{}
+	for i, tag := range englishTags {
+		// structpb.Struct 생성
+
+		filterStruct := &structpb.Struct{
+			Fields: map[string]*structpb.Value{
+				"ssss": structpb.NewStringValue(tag),
+			},
 		}
+		filterConditions[i] = filterStruct
+		// 입력받을 노래들의 리스트를 할당합니다
+		returnSongs := make([]songHomeResponse, 0, len(englishTags))
+
+		// 노래들을 입력을 받습니다
+		values, err := pineconeHandler.queryPineconeWithTag(filterConditions[i])
+		if err != nil {
+			BaseResponse(c, http.StatusInternalServerError, "error - "+err.Error(), nil)
+			return
+		}
+
+		// 받아온 입력들의 아이디 및 다른 값들을 할당합니다
+		for _, match := range values.Matches {
+			v := match.Vector
+			songNumber, err := strconv.Atoi(v.Id)
+			if err != nil {
+				log.Printf("Failed to convert ID to int, error: %+v", err)
+			}
+			koreanTags, err := mapTagsEnglishToKorean(parseTags(v.Metadata.Fields["ssss"].GetStringValue()))
+			if err != nil {
+				log.Printf("Failed to convert tags to korean, error: %+v", err)
+				koreanTags = []string{}
+			}
+			returnSongs = append(returnSongs, songHomeResponse{
+				SongNumber: songNumber,
+				SongName:   v.Metadata.Fields["song_name"].GetStringValue(),
+				SingerName: v.Metadata.Fields["singer_name"].GetStringValue(),
+				Tags:       koreanTags,
+			})
+		}
+		koreanTag, err := mapTagEnglishToKorean(tag)
+
 		homeResponses = append(homeResponses, HomeResponse{
-			Tag:   tag,
-			Songs: songs,
+			Tag:   koreanTag,
+			Songs: returnSongs,
 		})
 	}
+
 	BaseResponse(c, http.StatusOK, "ok", homeResponses)
 	return
 }
