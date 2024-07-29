75c05ff5dab054a2615eea4d1880cbb737687ba3
hectick <ch.yeondev@gmail.com>
2024-07-29 13:04:49 +0900

    feat: .env to comment

--- conf/config.go
+++ conf/config.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 	"github.com/pinecone-io/go-pinecone/pinecone"
 	"github.com/redis/go-redis/v9"
 	"log"
@@ -15,10 +15,11 @@ import (
 func SetupConfig(ctx context.Context, db **sql.DB, rdb **redis.Client, idxConnection **pinecone.IndexConnection) {
 	var err error
 	// MySQL 설정
-	err = godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file") //개발환경용
-	}
+
+	//err = godotenv.Load(".env")
+	//if err != nil {
+	//	log.Printf("Error loading .env file")
+	//}
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
