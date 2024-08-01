1761b99fc0e2fd901b744cb2189c2701a991fde6
Sunwu Park <52268188+sunwupark@users.noreply.github.com>
2024-08-01 14:07:42 +0900

    Feat: pull request on master

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/pinecone-io/go-pinecone/pinecone"
 	"github.com/redis/go-redis/v9"
-	log "github.com/sirupsen/logrus"
+	"log"
 )
 
 // @title           싱송생송 API
@@ -21,17 +21,12 @@ import (
 func main() {
 	ctx := context.Background()
 
-	log.SetFormatter(&log.JSONFormatter{})
-
 	var db *sql.DB
 	var rdb *redis.Client
 	var idxConnection *pinecone.IndexConnection
 	conf.SetupConfig(ctx, &db, &rdb, &idxConnection)
 
 	r := router.SetupRouter(db, rdb, idxConnection)
-
-	log.WithFields(log.Fields{"string": "foo", "int": 1, "float": 1.1}).Info("My first event from golang to stdout")
-	log.WithFields(log.Fields{"string": "231", "int": 2, "float": 2.1}).Error("My second event from golang to stdout")
 
 	// 서버 실행
 	if err := r.Run(); err != nil {
