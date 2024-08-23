eeef389b12efca1a26f39df6fceb8239faf8a2b9
sunwupark <sunwu5678@gmail.com>
2024-08-23 17:30:18 +0900

    Test: testing with deleting port

--- conf/config.go
+++ conf/config.go
@@ -56,8 +56,8 @@ func SetupConfig(ctx context.Context, db **sql.DB, rdb **redis.Client, idxConnec
 	//	log.Printf("Error loading .env file") //개발환경용
 	//}
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 
 	*db, err = sql.Open("mysql", dsn)
 	if err != nil {
