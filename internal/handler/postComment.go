5b1fdbd54efeb37540637992207afa6173149032
sunwupark <sunwu5678@gmail.com>
2024-10-09 20:41:51 +0900

    Feat: getCommentOnPost Param description edited postId -> PostCommentId

--- internal/handler/postComment.go
+++ internal/handler/postComment.go
@@ -118,7 +118,7 @@ type PostCommentWithCounts struct {
 // @Accept       json
 // @Produce      json
 // @Param        postId   path     int  true  "Post ID"
-// @Param        cursor query int false "마지막에 조회했던 커서의 postId(이전 요청에서 lastCursor값을 주면 됨), 없다면 default로 가장 먼저 작성된 댓글부터 조회"
+// @Param        cursor query int false "마지막에 조회했던 커서의 postCommentId(이전 요청에서 lastCursor값을 주면 됨), 없다면 default로 가장 먼저 작성된 댓글부터 조회"
 // @Param        size query int false "한번에 조회할 게시글 개수. 입력하지 않는다면 기본값인 20개씩 조회"
 // @Success      200 {object} pkg.BaseResponseStruct{data=GetPostCommentResponse} "Success"
 // @Router       /v1/posts/{postId}/comments [get]
