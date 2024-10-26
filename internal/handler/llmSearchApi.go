2fac5ce27d41c26af63f74e7d7eea42a0a949d53
hectick <ch.yeondev@gmail.com>
2024-10-26 18:05:42 +0900

    feat: change number of search result

--- internal/handler/llmSearchApi.go
+++ internal/handler/llmSearchApi.go
@@ -28,7 +28,7 @@ func GetSearchResultsForLLM(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		llmSearch, err := mysql.LLMSearchLogs(
 			qm.OrderBy("created_at DESC"),
-			qm.Limit(10),
+			qm.Limit(15),
 		).All(c.Request.Context(), db)
 		if err != nil {
 			pkg.BaseResponse(c, http.StatusInternalServerError, "error - "+err.Error(), nil)
@@ -42,7 +42,7 @@ func GetSearchResultsForLLM(db *sql.DB) gin.HandlerFunc {
 
 		// Get random 3 search texts
 		// 랜덤으로 3개의 텍스트 선택
-		randomTexts := getRandomElements(searchTexts, 3)
+		randomTexts := getRandomElements(searchTexts, 10)
 
 		pkg.BaseResponse(c, http.StatusOK, "success", SearchResultForLLMResponse{SearchTexts: randomTexts})
 	}
